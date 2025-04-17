780aed6a18eddefb777a4eb029b110e4a09db098
DevByRayRay <raymon@byrayray.dev>
2025-04-17 22:53:40 +0200

    feat: fix app crash, update project configuration for version 0.3.0, add target optimizations, and enhance macOS settings

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // Add specific configuration for Mac builds
+    #[cfg(target_os = "macos")]
+    {
+        println!("cargo:rustc-link-lib=framework=WebKit");
+    }
+    
     tauri_build::build()
 }
