b767ce7346318beb38b39f37f3e014be88363b85
DevByRayRay <raymon@byrayray.dev>
2025-04-20 13:38:35 +0200

    Setup Nuxt website

---
+++ website/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-black">
+    <h1>Test</h1>
+  </div>
+</template>
---
+++ website/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+export default defineNuxtConfig({
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	css: ['~/assets/css/main.css'],
+	modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	content: {
+		experimental: { nativeSqlite: true }
+	}
+})
